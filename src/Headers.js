e3c10a26d46311037afa05d2053e0d55ea734c3e
Alex88alex <alex88shlikov@gmail.com>
2018-12-12 23:33:54 +0200

    1

--- src/Headers.js
+++ src/Headers.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import PropTypes from "prop-types";
+//import PropTypes from "prop-types";
 
 class Headers extends Component {
     //  static propTypes =  {   неработает..
