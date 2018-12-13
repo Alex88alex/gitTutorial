0db4e0d6b0ba463dafc916753183b92e3e37d520
Alex88alex <alex88shlikov@gmail.com>
2018-12-13 23:34:58 +0200

    13.12

--- src/App.js
+++ src/App.js
@@ -24,6 +24,15 @@ const menu = [
   }
 ];
 
+//let - обычная переменная
+//const - переменная является константой - фактически постоянна и неизменяема
+//var - устарело
+
+if(true){
+ var a = "ES5";
+}
+console.log(a);
+
 const num1 = [1,2,3,4];
 const num2 = [5,6,7,8];
 //const res = [num1,num2];
@@ -42,12 +51,61 @@ let c ={
 };
 console.log(c);
 
-//const arr = [1,2,3];
-//function sum(a,b,c){
- // return a+b+c;
-//}
+const arr = [1,2,3];
+function sum(a,b,c){
+  return a+b+c;
+}
+console.log(sum(...arr));
 
+function res(arg,arg2,...REST){
+  console.log(arg,arg2,REST);
+}
+res (1,2,3,4,5,6,7);
 
+//шаблонная строка(?Template string)
+
+function greet(name){
+//  console.log('Hello'+name+'!');
+console.log('Hello${name}!');
+}
+greet('Max');
+const URL = '/api';
+let path1='test';
+let path2='test2';
+let path3='test3';
+
+//const API_URL = URL+'/'+path1+'/'+path2+'/'+path3;
+const API_URL = '${URL}/${path1}/${path2}/${path3}';
+console.log(API_URL);
+//чтото тут неработает
+
+//   Стрелочная функция
+
+//function greed(name,age){
+//  return'Hello ${name}, your age is ${age}!';
+//};
+//console.log(greed('Max',20));
+
+//()=>{}
+const greed = (name,age) =>'Hello ${name},your name is ${age}!'
+
+const FUNC = () => {};
+
+(a,b)=>{
+  let sun = a+b;
+  return sum;
+};
+
+(a)=>{
+  let sum=a+1;
+  return sum;
+};
+
+a=>a*2;
+(a,b)=>a*b;
+
+//самовызыв.функция
+(()=>{nul})();
 
 class App extends Component {
   render() {
